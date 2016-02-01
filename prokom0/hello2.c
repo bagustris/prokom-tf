5e62a0740b77ce487a2a05d7a369243e1d716f7b
bagustris <bagustris@yahoo.com>
2016-02-01 20:48:06 +0700

    second commit

---
+++ prokom0/hello2.c
@@ -0,0 +1,11 @@
+// demoed fungsi scanf untuk mengambil input
+
+#include <stdio.h>
+
+int main(void)
+{
+	char nama[5];    
+	printf("Masukkan nama anda: ");
+    scanf("%s", nama);
+    printf("hello, %s\n", nama);
+}
