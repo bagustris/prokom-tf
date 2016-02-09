ab739464ec73ef7201bd1db3ecfa177c87a96d71
bagustris <bagustris@linuxmail.org>
2016-02-09 20:55:13 +0700

    second big commit

---
+++ prokom2/doWhile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main()
+{
+	int n;
+  
+	do
+	{
+ 	printf("Masukkan angka lebih dari 100: ");
+  	scanf("%i", &n);  
+	}
+
+	while (n <= 100);
+	{
+		printf("hello world\n");
+	}
+	
+  	return 0;
+}
