2d223885b8a445b6f541055fefddc786ed8e388a
bagustris <bagustris@linuxmail.org>
2016-02-24 09:01:52 +0700

    add .c

---
+++ prokom1/forBintang.c
@@ -0,0 +1,13 @@
+/* forBintang.c
+ * demonstrated for to print star
+ * 02/23/2016
+ * bagustris, bagustris@yahoo.com
+ */
+#include <stdio.h>
+
+int main(void)
+{
+  for (int i=0; i<=10; i++)
+    printf("*");
+  //printf("\n");
+}
---
+++ prokom1/forBintang3.c
@@ -0,0 +1,20 @@
+/* forBintang3.c
+ * demonstrated for with increament
+ * 02/23/2016
+ * bagustris, bagustris@yahoo.com
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int i,j,a;
+  for (i=1; i<=10; i++)
+  {
+    for (j=1; j<=i; j++)
+      printf("*");
+    a=1;
+    a=a+1;
+      printf("\n");
+  }
+}
